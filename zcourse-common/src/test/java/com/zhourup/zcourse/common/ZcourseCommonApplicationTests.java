2c24ae616cb10fe219738ef712dac99c490603ca
zhourup <zhourup@163.com>
2022-03-21 10:34:07 +0800

    API网关服务和公共服务初始化

---
+++ zcourse-common/src/test/java/com/zhourup/zcourse/common/ZcourseCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhourup.zcourse.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZcourseCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
